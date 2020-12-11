05d9f741b71f6ba9bf6ed7452c4dceb9cdf6c774
Paul <shinpe@uci.edu>
2020-12-11 09:14:28 -0800

    Current boundary setup

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ master = mavutil.mavlink_connection('tcp:localhost:11520')
 
 master.wait_heartbeat(blocking=True)
 
-
 wp = mavwp.MAVWPLoader()
 seq = 1
 frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
 radius = 1
 for i in range(N):
-            wp.add(mavutil.mavlink.MAVLink_mission_item_message(master.target_system,
-                         master.target_component,
-                         seq,
-                         frame,
-                         mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-                         0, 0, 0, radius, 0, 0,
-                         lat[i],lon[i],alt[i]))
-            seq += 1
+    wp.add(mavutil.mavlink.MAVLink_mission_item_message(master.target_system,
+                                                        master.target_component,
+                                                        seq,
+                                                        frame,
+                                                        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                                                        0, 0, 0, radius, 0, 0,
+                                                        lat[i], lon[i], alt[i]))
+    seq += 1
+
+boundary = mavwp.MAVFenceLoader()
+seq2 = 1
+for i in range(M):
+    boundary.add(mavutil.mavlink.MAVLink_mission_item_message(master.target_system,
+                                                              master.target_component,
+                                                              seq2,
+                                                              frame))
+
+
+
+
 
 master.waypoint_clear_all_send()
 master.waypoint_count_send(wp.count())
