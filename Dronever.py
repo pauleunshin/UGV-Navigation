68265450adf31ae6d651f758ce0d3dc5295992e6
Paul <shinpe@uci.edu>
2021-02-22 19:47:45 -0800

    2/22 Dronekit work

--- Dronever.py
+++ Dronever.py
@@ -2,6 +2,24 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative, Command, Loca
 from pymavlink import mavutil
 
 
+#Connect to rover
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+connection_string = args.connect
+
+
+#SITL -- Comment out if connecting to actual UGV
+import dronekit_sitl
+sitl=dronekit_sitl_start_default()
+connection_string=sitl.connection_string()
+
+print("Connecting to UGV")
+vehicle.connect(connection_string, wait_ready=True)
+
+
+
 #Arms Rover to Move at altitude of sea level
 def arm(altitude):
 
