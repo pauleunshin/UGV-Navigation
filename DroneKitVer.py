b7ad17637f2eb303503562e755c08914a83d1378
Paul <shinpe@uci.edu>
2021-05-20 13:04:14 -0700

    Boundary removal temp

--- DroneKitVer.py
+++ DroneKitVer.py
@@ -15,12 +15,16 @@ connection_string = args.connect
 # SITL -- Comment out if connecting to actual UGV
 import dronekit_sitl
 
-sitl = dronekit_sitl_start_default()
-connection_string = sitl.connection_string()
+sitl = None
 
-print("Connecting to UGV")
-vehicle.connect(connection_string, wait_ready=True)
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
 
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
 
 # Functions
 def arm():
@@ -45,6 +49,46 @@ def clearmission(rover):
     cmds.download()
     cmds.wait_read()
 
+def get_location_metres(original_location, dNorth, dEast):
+    earth_radius=6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+    return LocationGlobal(newlat, newlon,original_location.alt)
+
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+
+
+def distance_to_current_waypoint():
+    """
+    Gets distance in metres to the current waypoint.
+    It returns None for the first waypoint (Home location).
+    """
+    nextwaypoint = vehicle.commands.next
+    if nextwaypoint==0:
+        return None
+    missionitem=vehicle.commands[nextwaypoint-1] #commands are zero indexed
+    lat = missionitem.x
+    lon = missionitem.y
+    alt = missionitem.z
+    targetWaypointLocation = LocationGlobalRelative(lat,lon,alt)
+    distancetopoint = get_distance_metres(vehicle.location.global_frame, targetWaypointLocation)
+    return distancetopoint
+
+
+
 
 def download_mission(rover):
     cmds = rover.commands
@@ -52,11 +96,41 @@ def download_mission(rover):
     cmds.wait_read()
 
 
-def get_mission(rover):
-    # --- Downloads mission#
-    print("Downloading Mission")
-    missionlist = []
-    n_wp = 0
+def adds_square_mission(aLocation, aSize):
+    """
+    Adds a takeoff command and four waypoint commands to the current mission.
+    The waypoints are positioned to form a square of side length 2*aSize around the specified LocationGlobal (aLocation).
+
+    The function assumes vehicle.commands matches the vehicle mission state
+    (you must have called download at least once in the session and after clearing the mission)
+    """
+
+    cmds = vehicle.commands
+
+    print(" Clear any existing commands")
+    cmds.clear()
+
+    print(" Define/add new commands.")
+    # Add new commands. The meaning/order of the parameters is documented in the Command class.
+
+    # Add MAV_CMD_NAV_TAKEOFF command. This is ignored if the vehicle is already in the air.
+    cmds.add(
+        Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0,
+                0, 0, 0, 0, 10))
+
+    # Define the four MAV_CMD_NAV_WAYPOINT locations and add the commands
+    point1 = get_location_metres(aLocation, aSize, -aSize)
+    cmds.add(
+        Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0,
+                0, 0, 0, point1.lat, point1.lon, 11))
+    # add dummy waypoint at point 1 (lets us know when have reached destination)
+    cmds.add(
+        Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0,
+                0, 0, 0, point1.lat, point1.lon, 14))
+
+    print(" Upload new commands to vehicle")
+    cmds.upload()
+
 
 # Vehicle Execution
 vehicle = connectMyCopter()
